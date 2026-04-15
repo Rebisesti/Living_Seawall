f07573be510c3a93dc0a6bf8711c62976ed458ce
Rebisesti <rbisesti@mail.endicott.edu>
2026-04-15 17:16:30 -0400

    update5

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// slideshow function
+let intIndex = 0;
+
+function showImage(i) {
+    intIndex += i;
+
+    let theImages = document.getElementsByClassName("image");
+    let theDots = document.getElementsByClassName("dot");
+
+    for (let n = 0; n < theImages.length; n++) {
+        theImages[n].style.display = "none";
+        theDots[n].className = theDots[n].className.replace(" active", "")
+    }
+
+    if (intIndex > theImages.length - 1) {
+        intIndex = 0;
+    }
+
+    if (intIndex < 0) {
+        intIndex = theImages.length - 1;
+    }
+
+    theImages[intIndex].style.display = "block";
+    theDots[intIndex].className += " active";
+}
